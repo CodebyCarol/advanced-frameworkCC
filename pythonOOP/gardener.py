025dd86552ac92d9c093e284df772bbf390a5e2c
Kata <katalin.molnar.kiss@gmail.com>
2018-06-26 13:22:49 +0200

    gardener OOP practice

---
+++ pythonOOP/gardener.py
@@ -0,0 +1,8 @@
+class Plant:
+
+    def __init__(self, size, limit_of_proliferation, name):
+        self.size = size
+        self.limit_of_proliferation = limit_of_proliferation
+        self.name = name
+
+
