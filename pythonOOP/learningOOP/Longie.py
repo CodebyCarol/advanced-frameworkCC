45a9e7f2ebf89b13b222fd771f476cb3cf8ab919
Kata <katalin.molnar.kiss@gmail.com>
2018-06-27 14:45:00 +0200

    method override

---
+++ pythonOOP/learningOOP/Longie.py
@@ -0,0 +1,22 @@
+from woman import Blondie
+
+
+class Longie(Blondie):
+    'try to reach parent class from Longie (the long hair blondies)'
+
+    long = 50
+
+    def __init__(self, name, long):
+        self.name = name
+        self.long = long
+        haircolor = super
+        print("initialize Longie")
+
+    # override
+    def blondie_method(self):
+        print("this is a very unique Blondie method overrided in Longie")
+
+longie1 = Longie("Rapunzel", 400)
+
+print(longie1.haircolor)        
+longie1.blondie_method() 
--- pythonOOP/learningOOP/woman.py
+++ pythonOOP/learningOOP/woman.py
@@ -1,3 +1,5 @@
+# in Python you refer to the .py file where from you import class name!
+# (because you dont have to give the same name to the class and to the file...)
 from learn import Person 
 
 
@@ -14,6 +16,31 @@ woman1 = Woman("Sara")
 
 woman1.childMethod()
 woman1.display_count()
+print("Woman.__bases__:  ", Woman.__bases__)
+print("Woman.__module__:", Woman.__module__)
 
 print(woman1.name + "also can call the parent class, so she can tell that already " +
                     (str)(woman1.count_dudes) + " instance has beencreated")
+
+
+class Blondie(Woman):
+
+    haircolor = "blonde"
+
+    def __init__(self, name):
+        self.name = name
+        print("initialize Blondies")
+
+    def blondie_method(self):
+        print("method of the blondie class")
+
+    def display_haircolor(self):
+        print("Haircolor: " + self.haircolor)
+
+    def blondie_method(self):
+        print("this is a very unique Blondie method")    
+
+blondie1 = Blondie("Monica")
+
+print(blondie1.name, "'s hair is: ", blondie1.haircolor)
+print("bases: ", Blondie.__bases__)
