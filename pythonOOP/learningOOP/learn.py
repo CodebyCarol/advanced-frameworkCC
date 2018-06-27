473d4153ab49ad2322a2a2bb0da5dbd0370a2eb5
Kata <katalin.molnar.kiss@gmail.com>
2018-06-27 13:24:56 +0200

    instantiating is working :)

--- pythonOOP/learningOOP/learn.py
+++ pythonOOP/learningOOP/learn.py
@@ -1,11 +1,30 @@
 class Person:
+    'it is a very basic excercise'
     
+    count_dudes = 0
+    dudes = []
+
     def __init__(self, name):
         self.name = name
+        Person.count_dudes += 1
+        Person.dudes.append(name)
 
-    def say_hello(self):
-        print("Hello, my name is " + self.name)
+    def display_count(self):
+        print("The total number of instances (dudes): %d" % Person.count_dudes)
 
-    person = Person("Newbie")
+    def display_dude_name(self):
+        print("Name: " + self.name)
 
-    person.say_hello()    
+    def display_dudes(self):
+        print(self.dudes)
+
+
+dude1 = Person("Newbie")
+dude2 = Person("Oldie")
+
+dude1.display_count()
+dude1.display_dude_name()
+dude1.display_dudes()
+    # def say_hello(self):
+    #     print("Hello, my name is " + self.name)
+
