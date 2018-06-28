38e564f2d167de9ba7d2599575593bbf3fdd1221
Kata <katalin.molnar.kiss@gmail.com>
2018-06-28 11:09:37 +0200

    in homework librarian tells his name

--- pythonOOP/python_practice/librarian.py
+++ pythonOOP/python_practice/librarian.py
@@ -1,7 +1,9 @@
 class Librarian:
 
+    name = "Librarian"
+
     def __init__(self, name):
         self.name = name
     
     def tell_the_name(self):
-        print(self.name)
+        return self.name
--- pythonOOP/python_practice/main.py
+++ pythonOOP/python_practice/main.py
@@ -1,5 +1,5 @@
 from book import Book
-
+from librarian import Librarian
 
 book1 = Book("Lord of the Rings", 3000)
 book2 = Book("Rings of the Lords", 2900)
@@ -9,3 +9,7 @@ book1.list_all_books()
 # when you calling a static method, you should whrite the instances to replace "self"
 book1.say_hello_to_librarian(book1)
 book1.gross_price()
+
+librarian = Librarian("Philipe")
+
+print("Hello, my name is", librarian.tell_the_name())
