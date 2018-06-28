f04b0e313f11595c4c3859b803895e0e921f3d6d
Kata <katalin.molnar.kiss@gmail.com>
2018-06-28 10:59:41 +0200

    pep8

--- pythonOOP/python_practice/main.py
+++ pythonOOP/python_practice/main.py
@@ -6,6 +6,6 @@ book2 = Book("Rings of the Lords", 2900)
 book1.read_up_itself()
 book2.read_up_itself()
 book1.list_all_books()
-#when you calling a static method, you should whrite the instances to replace "self"
+# when you calling a static method, you should whrite the instances to replace "self"
 book1.say_hello_to_librarian(book1)
 book1.gross_price()
